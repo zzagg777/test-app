611d625e2090137d3e0282a793d3ed6552c1aeec
min <zzagg777@gmail.com>
2026-03-17 13:06:24 +0900

    init : vite react project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return <div>Hello React 👋</div>;
+}
+
+export default App;
